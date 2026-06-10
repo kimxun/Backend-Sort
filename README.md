fa8ce2a3dc038cf00beb031aba3b1daa1cb09e08
xunv09 <0306231352@caothang.edu.vn>
2026-06-10 17:21:10 +0700

    Setup Flask backend and database configuration

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Backend-Sort
+Cài đặt Python dependencies
+
+```bash
+# Tạo virtual environment (khuyến nghị)
+python -m venv venv
+
+# Kích hoạt virtual environment
+# Windows:
+venv\Scripts\Activate.ps1
+# Linux/Mac:
+source venv/bin/activate
+
+# Cài đặt dependencies
+python -m pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+from config import Config
 from algorithms.interchange_sort import interchange_sort_logic
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config.from_object(Config)
 CORS(app)
+db.init_app(app)  
 
+print("CONFIG DB =", app.config["SQLALCHEMY_DATABASE_URI"])
 algorithms_db = {
     "interchange": {
         "title": "Interchange Sort",
@@ -25,30 +30,38 @@ algorithms_db = {
 }
 
 
-@app.route('/api/algorithms', methods=['GET'])
-def get_algorithms():
-    return jsonify(algorithms_db)
+# @app.route('/api/algorithms', methods=['GET'])
+# def get_algorithms():
+#     return jsonify(algorithms_db)
 
 
-@app.route('/api/sort', methods=['POST'])
-def sort_array():
-    data = request.get_json()
+# @app.route('/api/sort', methods=['POST'])
+# def sort_array():
+#     data = request.get_json()
 
-    array = data.get("array", [])
-    algorithm = data.get("algorithm", "interchange")
+#     array = data.get("array", [])
+#     algorithm = data.get("algorithm", "interchange")
 
-    if algorithm == "interchange":
-        steps = interchange_sort_logic(array)
-        return jsonify({
-            "success": True,
-            "steps": steps
-        })
+#     if algorithm == "interchange":
+#         steps = interchange_sort_logic(array)
+#         return jsonify({
+#             "success": True,
+#             "steps": steps
+#         })
 
-    return jsonify({
-        "success": False,
-        "message": "Thuật toán chưa được hỗ trợ"
-    }), 400
+#     return jsonify({
+#         "success": False,
+#         "message": "Thuật toán chưa được hỗ trợ"
+#     }), 400
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+      with app.app_context():
+        try:
+            db.session.execute(db.text("SELECT 1"))
+            print("✅ Kết nối MySQL thành công!")
+        except Exception as e:
+            print("❌ Kết nối MySQL thất bại!")
+            print(e)
+    
+app.run(debug=True, port=5000)
